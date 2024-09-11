eefb07fcf0552f830d64a61806290abe38446166
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2024-09-11 16:18:23 +0500

    create module capabilities

---
+++ db/access.php
@@ -0,0 +1,33 @@
+<?php
+$capabilities = [
+    'mod/onlyofficedocspace:addinstance' => [
+        'riskbitmask' => RISK_XSS,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => [
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+        ],
+        'clonepermissionsfrom' => 'moodle/course:manageactivities',
+    ],
+    'mod/onlyofficedocspace:edit' => [
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_MODULE,
+        'archetypes' => [
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+            'guest' => CAP_PROHIBIT,
+        ],
+    ],
+    'mod/onlyofficedocspace:view' => [
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_MODULE,
+        'archetypes' => [
+            'student' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+        ],
+    ],
+];
