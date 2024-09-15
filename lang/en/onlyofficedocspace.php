1948be4e43e2e6540ac9ed35541abdb899cd02a2
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2024-09-16 00:22:52 +0500

    create en lang file

---
+++ lang/en/onlyofficedocspace.php
@@ -0,0 +1,66 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Languages configuration for the mod_[modname] plugin.
+ *
+ * @package   mod_onlyofficedocspace
+ * @copyright Year, You Name <your@email.address>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['adminsettings:updated'] = 'ONLYOFFICE DocSpace settings has been updated.';
+$string['adminsettings:urldescription'] = "The DocSpace Service Address specifies the address of the server with the document services installed. <br/> Please replace 'https://docspaceserver.url' above with the correct server address";
+$string['modulename'] = 'ONLYOFFICE DocSpace';
+$string['modulenameplural'] = 'ONLYOFFICE DocSpace';
+$string['pluginname'] = 'ONLYOFFICE DocSpace';
+$string['pluginadministration'] = 'ONLYOFFICE DocSpace activity administration';
+$string['activityname'] = 'Activity Name';
+$string['selectelement'] = 'Connect existing DocSpace room or file or create new one';
+$string['selectroom'] = 'Select room';
+$string['selectfile'] = 'Select file';
+$string['settings'] = 'ONLYOFFICE DocSpace Settings';
+$string['loginmodal:title'] = 'Moodle requests access to your ONLYOFFICE DocSpace';
+$string['loginmodal:description'] = 'Please enter your DocSpace password';
+$string['validationerror:password'] = 'Password you entered is incorrect';
+$string['docspaceserverurl'] = 'DocSpace Service Address';
+$string['documentserverurldescription'] = 'The DocSpace Service Address specifies the address of the server ';
+$string['docspacelogin'] = 'DocSpace Login';
+$string['docspacepassword'] = 'DocSpace Password';
+$string['docspaceuserscategory:title'] = 'DocSpace users';
+$string['docspaceunreachable'] = 'DocSpace is unavailable.';
+$string['onlyofficedocspace:addinstance'] = 'Add a new ONLYOFFICE DocSpace activity';
+$string['onlyofficedocspace:editdocument'] = 'Edit ONLYOFFICE DocSpace activity';
+$string['onlyofficedocspace:view'] = 'View ONLYOFFICE DocSpace activity';
+$string['selecteditemtype:room'] = 'Selected room';
+$string['selecteditemtype:file'] = 'Selected file';
+$string['logintodocspace'] = "Log in to ONLYOFFICE DocSpace";
+$string['invitetodocspace'] = 'Invite to DocSpace';
+$string['docspaceusernotfound'] = 'The user was not found in ONLYOFFICE DocSpace. Please, contact the administrator.';
+$string['docspaceuserscategory:description'] = 'To add new users to ONLYOFFICE DocSpace and to start working with plugin, please press <b>{$a}</b>. <br/> All new users will be added with <b>Power user</b> role, you can change the role in Accounts settings in DocSpace. <b>Power user</b> role is paid!';
+$string['paramsmissingvalidationerror'] = 'Required params are missing';
+$string['docspacepermissiondenied'] = 'The user does not have permission.';
+$string['docspaceautherror'] = 'The authentication process has failed.';
+$string['docspacefilenotfound'] = 'The required file was not found. Please check if the file still exist or contact the administrator.';
+$string['docspaceroomnotfound'] = 'The required room was not found. Please check if the room still exist or contact the administrator.';
+$string['enterfullscreen'] = 'Open full screen';
+$string['exitfullscreen'] = 'Exit full screen';
+$string['skippedinvitations'] = 'Invitation skipped for {$a} user(s). User(s) with the indicated email(s) may already exist in DocSpace.';
+$string['sentinvitations'] = 'Invitation successfully sent to {$a} user(s)';
+$string['failedinvitations'] = 'Invitation failed for {$a} user(s)';
+$string['docspaceuserinviteerror'] = 'Invitation process has failed.';
+$string['onlyofficedocspace:edit'] = 'Edit ONLYOFFICE DocSpace activity';
+$string['adminsettings:urlwarning'] = 'Changing the current server url will cause the current docspace users database to get deleted. Please, confirm your choice.';
