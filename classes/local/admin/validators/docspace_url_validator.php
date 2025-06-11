6b441a6c22e8b6ac940151e77ae743cf44c0abba
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2025-06-11 14:56:14 +0500

    create a docspace url validator class

---
+++ classes/local/admin/validators/docspace_url_validator.php
@@ -0,0 +1,70 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * DocSpace settings validator.
+ *
+ * @package    mod_onlyofficedocspace
+ * @copyright  2025 Ascensio System SIA <integration@onlyoffice.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_onlyofficedocspace\local\admin\validators;
+
+use mod_onlyofficedocspace\local\docspace\docspace_settings;
+
+/**
+ * DocSpace settings validator.
+ *
+ * @package    mod_onlyofficedocspace
+ * @copyright  2025 Ascensio System SIA <integration@onlyoffice.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class docspace_url_validator extends validator {
+
+    /**
+     * DocSpace URL.
+     *
+     * @var string
+     */
+    private string $url;
+
+    /**
+     * Constructor.
+     *
+     * @var string $url DocSpace url
+     */
+    public function __construct(string $url) {
+        $this->url = $url;
+    }
+
+    /**
+     * Validate DocSpace settings.
+     *
+     * @return void
+     */
+    public function validate() {
+        if (empty($this->url)) {
+            $this->errors[] = get_string('docspaceurlrequired', 'onlyofficedocspace');
+            return;
+        }
+
+        if (docspace_settings::healthcheck() === false) {
+            $this->errors[] = get_string('docspaceunreachable', 'onlyofficedocspace');
+            return;
+        }
+    }
+}
