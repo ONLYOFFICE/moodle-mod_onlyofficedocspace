ca931032a506384e917990bc5f90beb50258e39f
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2025-08-18 22:41:33 +0500

    create a simple url parser

---
+++ classes/local/utils/url_parser.php
@@ -0,0 +1,52 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Define a simple url parser class
+ *
+ * @package    mod_onlyofficedocspace
+ * @copyright   2025 Ascensio System SIA <integration@onlyoffice.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_onlyofficedocspace\local\utils;
+
+/**
+ * url_parser
+ */
+class url_parser {
+
+
+    /**
+     * get the base of the url
+     *
+     * @param string $url
+     * @return string
+     */
+    public static function get_base(string $url): string {
+        $scheme = parse_url($url, PHP_URL_SCHEME);
+        $host   = parse_url($url, PHP_URL_HOST);
+        $port   = parse_url($url, PHP_URL_PORT);
+
+        $baseurl = $scheme . '://' . $host;
+
+        if ($port) {
+            $baseurl .= ':' . $port;
+        }
+
+        return $baseurl;
+    }
+}
