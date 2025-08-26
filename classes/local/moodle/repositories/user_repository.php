81388420247bea9a6b0d24b1ee320b2d2dfc06e7
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2025-08-27 00:38:00 +0500

    add new get users with editor roles method

--- classes/local/moodle/repositories/user_repository.php
+++ classes/local/moodle/repositories/user_repository.php
@@ -93,6 +93,45 @@ class user_repository {
     }
 
     /**
+     * Return users with editor roles
+     * @param int $offest
+     * @param int $limit
+     * @return array
+     */
+    public function get_users_with_editor_roles(int $offest = 0, int $limit = 0) {
+        $context = context_system::instance();
+
+        $systemroles = get_assignable_roles($context);
+        $courseroles = array_filter(
+            get_default_enrol_roles($context),
+            fn($role) => strtolower($role) !== 'student'
+        );
+        $archetypes = $systemroles + $courseroles;
+
+        $extrasql = "id IN (SELECT userid FROM {role_assignments} a WHERE a.contextid= "
+            . SYSCONTEXTID . " AND a.roleid IN (" . implode(",", array_keys($systemroles)) . ") "
+            . "UNION SELECT userid FROM {role_assignments} a "
+            . "INNER JOIN {context} b ON a.contextid=b.id WHERE b.contextlevel="
+            . CONTEXT_COURSE . " AND a.roleid IN (" . implode(",", array_keys($courseroles)) . "))";
+
+        $users = get_users_listing(
+            sort: 'firstname',
+            page: $offest,
+            recordsperpage: $limit,
+            extraselect: $extrasql,
+            extracontext: $context
+        );
+
+        foreach ($users as &$user) {
+            $roles = $this->persistence->get_records('role_assignments', ['userid' => $user->id], fields: 'id,roleid');
+            $roles = array_unique(array_map(fn($role) => $role->roleid, $roles));
+            $user->role = implode(',', array_intersect_key($archetypes, $roles));
+        }
+
+        return $users;
+    }
+
+    /**
      * Get multiple users by their ids
      * @param array $ids
      * @return array
